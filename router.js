67a9461dd95eb038e99dc0b70c2b8b0a715c9f22
Himanshu Sharma <sharma-himanshu@outlook.com>
2017-07-15 10:07:45 -0500

    Initial Commit.

---
+++ router.js
@@ -0,0 +1,29 @@
+const AuthenticationController = require('./controllers/authentication');
+const express = require('express');
+const passport = require('passport');
+const passportService = require('./config/passport');
+
+// Middleware to require login/auth
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireLogin = passport.authenticate('local', { session: false });
+
+module.exports = function (app) {
+  // Initializing route groups
+  const apiRoutes = express.Router(),
+    authRoutes = express.Router();
+  //= ========================
+  // Auth Routes
+  //= ========================
+
+  // Set auth routes as subgroup/middleware to apiRoutes
+  apiRoutes.use('/auth', authRoutes);
+
+  // Registration route
+  authRoutes.post('/register', AuthenticationController.register);
+
+  // Login route
+  authRoutes.post('/login', requireLogin, AuthenticationController.login);
+
+  // Set url for API group routes
+  app.use('/api', apiRoutes);
+};
